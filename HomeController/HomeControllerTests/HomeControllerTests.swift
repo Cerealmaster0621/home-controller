39cb7ba5b4bad3253ce3e914ee255f3526e31770
Cerealmaster0621 <cerealmaster@naver.com>
2025-12-01 12:03:37 +0900

    mobile init, gitignored seperated

---
+++ HomeController/HomeControllerTests/HomeControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeControllerTests.swift
+//  HomeControllerTests
+//
+//  Created by YoungJune Kang on 2025/11/28.
+//
+
+import Testing
+@testable import HomeController
+
+struct HomeControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
